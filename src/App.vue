19f4afe858b3714045024899b07afef5b6232b24
Your Name <yes@qq.com>
2020-02-19 15:25:23 +0800

    项目开始--elementUI导入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <router-link to="#">123</router-link>
+    <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <h3>{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
   </div>
 </template>
 
